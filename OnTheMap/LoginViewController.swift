a270a0f8af340580c802fba07df6261d106c2387
woogii <jhsw0201@naver.com>
2015-11-16 07:33:31 +0900

    Configure layout of Loginview

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Hyun on 2015. 11. 15..
+//  Copyright © 2015년 wook2. All rights reserved.
+//
+
+import UIKit
+
+
+class LoginViewController : UIViewController {
+    
+    
+    @IBOutlet weak var loginBackgroundImage: UIImageView!
+   
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        //loginBackgroundImage.image = UIImage(named: "udacitylogo")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
